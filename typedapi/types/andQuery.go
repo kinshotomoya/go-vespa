7497672a3594e3592eb72916e84e3fbdf0a38b21
金正朋也 <mhtnim1023@gmail.com>
2023-08-15 20:23:11 +0900

    Feat: QueryType

--- typedapi/types/andQuery.go
+++ typedapi/types/andQuery.go
@@ -1,3 +1,15 @@
 package types
 
-// TODO: 続き
+import "strings"
+
+type AndQuery struct {
+	query []WhereQuery
+}
+
+func (a *AndQuery) BuildQuery() string {
+	queries := make([]string, len(a.query))
+	for i := range a.query {
+		queries[i] = a.query[i].BuildQuery()
+	}
+	return strings.Join(queries, " AND ")
+}
--- typedapi/types/query.go
+++ typedapi/types/query.go
@@ -1,9 +1,23 @@
 package types
 
+import (
+	"strings"
+)
+
 type Query struct {
-	Fields     string
+	Fields     []string
 	Schema     string
 	WhereQuery []WhereQuery
 	Limit      int
 	OffSet     int
+}
+
+func (q *Query) Build() string {
+	fields := strings.Join(q.Fields, ",")
+	queries := make([]string, len(q.WhereQuery))
+	for i := range q.WhereQuery {
+		queries[i] = q.WhereQuery[i].BuildQuery()
+	}
+	yqlSlice := []string{"SELECT", fields, "FROM", q.Schema, "WHERE", strings.Join(queries, " AND ")}
+	return strings.Join(yqlSlice, " ")
 }
