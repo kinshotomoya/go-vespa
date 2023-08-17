ff0486b2e4c7a48691cbb46ac1b4913db04e1525
金正朋也 <mhtnim1023@gmail.com>
2023-08-17 15:09:39 +0900

    Feat: Query Build method

--- typedapi/types/query.go
+++ typedapi/types/query.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -8,16 +9,26 @@ type Query struct {
 	Fields     []string
 	Schema     string
 	WhereQuery []WhereQuery
-	Limit      int
-	OffSet     int
+	Limit      *int
+	OffSet     *int
 }
 
 func (q *Query) Build() string {
-	fields := strings.Join(q.Fields, ",")
 	queries := make([]string, len(q.WhereQuery))
 	for i := range q.WhereQuery {
 		queries[i] = q.WhereQuery[i].BuildQuery()
 	}
-	yqlSlice := []string{"SELECT", fields, "FROM", q.Schema, "WHERE", strings.Join(queries, " AND ")}
-	return strings.Join(yqlSlice, " ")
+
+	// NOTE: Need a slice that can store a minimum of 6 and a maximum of 10 elements, including limit and offset
+	yqlSlice := make([]string, 6, 10)
+	yqlSlice = append(yqlSlice, "select", strings.Join(q.Fields, ","), "from", q.Schema, "where", strings.Join(queries, " and "))
+	if q.Limit != nil {
+		yqlSlice = append(yqlSlice, "limit", strconv.Itoa(*q.Limit))
+	}
+
+	if q.OffSet != nil {
+		yqlSlice = append(yqlSlice, "offset", strconv.Itoa(*q.OffSet))
+	}
+
+	return strings.Join(yqlSlice[:], " ")
 }
